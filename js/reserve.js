4b538d520bd3f8340c8d6099b7638ce27313389c
yuliah <148478120+yuliah900@users.noreply.github.com>
2023-12-11 14:20:29 +0700

    error in fetch

--- js/reserve.js
+++ js/reserve.js
@@ -10,19 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
     messageForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        const fullName = document.getElementById('fullName').value;
-        const email = document.getElementById('email').value;
+        const full_name = document.getElementById('fullName').value;
+        const email_address = document.getElementById('email').value;
         const address = document.getElementById('address').value;
-        const checkInDate = document.getElementById('checkInDate').value;
-        const checkOutDate = document.getElementById('checkOutDate').value;
-        const guestCount = document.getElementById('guestCount').value;
+        const check_in = document.getElementById('checkInDate').value;
+        const check_out = document.getElementById('checkOutDate').value;
         const type = document.getElementById('typeSelect').value;
-        const room = document.getElementById('roomSelect').value;
-        const payment = document.getElementById('paymentSelect').value;
-        const cardNumber = document.getElementById('cardNumber').value;
+        const rooms = document.getElementById('guestCount').value;
+        const guest = document.getElementById('roomSelect').value;
+        const name_card = document.getElementById('paymentSelect').value;
+        const card_number = document.getElementById('cardNumber').value;
         const cvv = document.getElementById('cvv').value;
 
-        if (!fullName || !email || !address || !checkInDate || !checkOutDate || !guestCount || !type || !room || !payment || !cardNumber || !cvv) {
+        if (!full_name || !email_address || !address || !check_in || !check_out || !type || !rooms || !guest || !name_card || !card_number ||!cvv) {
             showSweetAlert(
                 "Oops!",
                 "Please fill in all the fields before submitting.",
@@ -31,19 +31,27 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        const formData = {
-            fullName,
-            email,
-            address,
-            checkInDate,
-            checkOutDate,
-            guestCount,
-            type,
-            room,
-            payment,
-            cardNumber,
-            cvv,
-        };
+        const formData= new formData (document.getElementById("reservationForm"));
+        const requestBody = {};
+        
+         formData.forEach((value, key) => {
+            requestBody[key] = value;
+          });
+            requestBody.full_name=full_name;
+            requestBody.email_address = email;
+            requestBody.address = address;
+            requestBody.check_in = check_in;
+            requestBody.check_out = check_out;
+            requestBody.type = type;
+            requestBody.rooms = rooms;
+            requestBody.guest = guest;
+            requestBody.name_card = name_card;
+            requestBody.card_number = card_number;
+            requestBody.cvv = cvv;
+
+    
+           
+        
 
         // Kirim data ke backend
         fetch(`${API_URL}/reserve`, {
